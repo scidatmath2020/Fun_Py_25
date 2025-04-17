21eb84e54091cb3077409fa6f5c0041fa241617e
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2025-04-16 19:02:49 -0600

    Add files via upload

---
+++ scripts/sesion_07.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 17:17:19 2025
+
+@author: Usuario
+"""
+
+1 + 1
+
+15 * 4 + 1 / 3 ** 5 #  3^5
+
+2 ** 5 # 2^5, 2**5
+
+9 ** 0.5  
+
+98 / 4
+
+98 // 4
+
+98 % 4
+
+98 % 4
+
+### Formato HH:MM (23:30), (17:33)  
+### 
+ 
+17 % 12
+23 % 12
+
+
+# ¿Qué día de la semana es el 30 de abril?
+
+30 % 7
+29 % 7
+
+
+16 % 7
+
+
+12 * 5 + 2 / (4 ** 2)
+# 12*5+2/16
+# 60+2/16
+# 60+0.125
+# 60.125
+
+15 / 5 % 3
+# 3 % 3
+# 0
+
+15 / (5 % 3)
+# 15 / 2
+# 7.5
+
+(15 / 5) % 3
+# 3 % 3
+# 0
+
+(12 * 5) + (2 / (4 ** int("2")))
+
+# (12 * 5) + (2 / (4 ** 2))
+# (12 * 5) + (2 / 16)
+# (12 * 5) + 0.125
+# 60 + 0.125
+# 60.125
+
+#%%
+
+"hola" + "mundo"
+
+"hola" + " mundo"
+
+"hola " + "mundo"
+
+"hola" + " " + "mundo"
+
+"hola" + "          " + "mundo"
+
+"hola" + "423 &" + "mundo"
+
+[1, 2, 3] + ['uno', 'dos']
+
+(1, 2) + ('uno', 'dos',True)
+
+(1, 2) + [1, 2, 3]
+
+2 + "hola"
+
+(1,2) + "hoola"
+
+"Ella me romió el corazón en" + 999 + 1 + "pedazos"
+
+"Ella me romió el corazón en " + str(999 + 1) + " pedazos"
+
+respuesta = False
+
+if respuesta == True:
+    frase = "Ella me quiere"
+else:
+    frase = "Ella me romió el corazón en " + str(999 + 1) + " pedazos"
+
+print(frase)
+
+conectados_hoy = 0
+conectados_hoy_texto = str(conectados_hoy)
+"hola a los " + conectados_hoy_texto +" que están conectados"
+
+"hola "*5
+
+["a",2]*3
+
+("a",2,False)*4
+
+"*"*3
+
+for i in range(20):
+    print("o"*i)
+
+
+objeto_1 = "hola a todos."
+
+resultado_complicado = 15 * 4 + 1 / 3 ** 5
+
+objeto_2 = " ¿Cómo están?"
+
+objeto_1 = objeto_1 + objeto_2
+
+objeto_1 += objeto_2
+
+mi_numero = 20
+
+mi_numero += 30
+
+### mi_numero = mi_numero + 30
+
+
+tupla = (1, 2, 3)
+tupla += ('cuatro', 'cinco', 'seis')
+
+#%%
+
+"hola" == 'hola'
+
+2 + 5 == 7
+
+2 + 5 == 8
+
+"Hola" == "hola"
+
+Valor = "otra cosa"
+
+if Valor == "suma":
+    print(2+5)
+else:
+    print(2*5)
+
+"hola" != 'Hola'
+"hola" != 'hola'
+
+contador = 0
+
+while contador != 10:
+    contador += 1
+    print("hola")
+    
+    
+5 > 3    
+
+contador = 0
+
+while contador < 10:
+    contador += 1
+    print("hola")
+    
+    
+2+8 <= 10   
+    
+contador = 0
+
+while contador <= 10:
+    contador += 1
+    print("hola")
+    
+    
+lista1 = [11,11,5]
+lista2 = [11,11,5]
+lista3 = [11,11,5,25]
+
+lista1 == lista2
+lista1 == lista3
+
+lista1 < lista3
+
+### lista < lista
+
+### 0123456789ABC...Zabc...z
+
+"Hora" < "Hola"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
