54dd1292ed876eb8911b787e5c5a8887a26a747d
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2025-05-01 18:53:57 -0600

    Add files via upload

---
+++ scripts/sesion_modulos.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  1 18:28:26 2025
+
+@author: Usuario
+"""
+
+'''módulos en Python (paqueterías, librerías, bibliotecas)'''
+
+
+'''
+Un módulo es un conjunto de herramientas que sirven para un mismo fín y que
+alguien más ya programó
+'''
+
+'''En Python, para usar módulos se requiere de la instrucción import.
+
+Básicamente la aplicación de un módulo se realiza en dos pasos:
+    - Instalar el módulo  (Jaime le pide al ayuntamiento el libro)
+    - Cargarlo en la sesión (Jaime le da el libro a Juan)
+'''
+
+import pygame  # Juan le está pidiendo a Jaime el libro pygame
+
+
+''' en análisis de datos los módulos mas importantes son pandas, numpy, scipy, keras, tensorflow,
+siuba, matplotlib, plotnine, ....
+'''
+
+import pandas
+
+tabla = pandas.DataFrame({"col1":[1,3,3],"col2":["a","b","c"]})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
